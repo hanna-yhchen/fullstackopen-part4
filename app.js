22266b9f543757f6dee945b0299a79664e6064ce
Hanna Chen <hanna.yhchen@gmail.com>
2024-01-22 23:28:49 +0800

    Refactor into separate modules

---
+++ app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+async function connectToMongoDB() {
+  logger.info(`Connecting to ${config.MONGODB_URI}`)
+  try {
+    mongoose.set('strictQuery', false)
+    await mongoose.connect(config.MONGODB_URI)
+    logger.info('Successfully connected to MongoDB.')
+  } catch (error) {
+    logger.error(`Error connecting to MongoDB: ${error.message}`)
+  }
+}
+connectToMongoDB()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
